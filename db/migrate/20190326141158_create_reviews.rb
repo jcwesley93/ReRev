e57c83b23e8be7accff89c9566106ea03247bb90
Jaela Wesley <github email address>
2019-03-26 12:21:45 -0400

    migrated database

--- db/migrate/20190326141158_create_reviews.rb
+++ db/migrate/20190326141158_create_reviews.rb
@@ -1,7 +1,9 @@
 class CreateReviews < ActiveRecord::Migration[5.2]
   def change
     create_table :reviews do |t|
-
+      t.text :comment
+      t.integer :author_id
+      t.integer :submission_id
       t.timestamps
     end
   end
