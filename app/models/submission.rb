71869e9c3afdca0c3bf637a408c01000ba8464fe
Jaela Wesley <github email address>
2019-03-28 13:10:21 -0400

    added two model methods in genre and submission. Updated private profile page

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -5,4 +5,17 @@ class Submission < ApplicationRecord
   validates :title, :content, presence: true
   validates :title, length: {maximum: 150}
   validates :content, length: {minimum: 150}
+
+    def self.most_reviewed 
+      most_reviewed = 0 
+      most_reviewed_title = ""
+      self.all.each do |submission|
+        if submission.reviews.count > 0 && submission.reviews.count > most_reviewed
+          most_reviewed = submission.reviews.count
+          most_reviewed_title = submission.title
+         end
+      end
+      most_reviewed_title
+    end
+
 end
